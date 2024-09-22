c97279525fd4716589a9c86e9a51eb2706716911
Balan S.P <balansp@users.noreply.github.com>
2024-09-22 07:50:17 +0530

    Update Waterproof-SonarSensor.ino

--- Waterproof-SonarSensor.ino
+++ Waterproof-SonarSensor.ino
@@ -1,53 +1,52 @@
- 
 #include <Arduino.h>
+#include <Wire.h>
 #include <SoftwareSerial.h>
-/* 
-White - Green - RX
-Yellow - Blue - TX
-Red - Red - VCC
-Black - Black - GND
-*/
-SoftwareSerial mySerial(D2,D3); // RX, TX
-unsigned char data[4]={};
-float distance;
 
-int getDistance(){
-  do{
-     for(int i=0;i<4;i++){
-       data[i]=mySerial.read();
-     }
-  }while(mySerial.read()==0xff);
+/*
+ ---------------------------
+ | ESP     WIRE      SONAR |
+ ---------------------------
+ D0   = RED RED = VCC
+ GND  = BLK BLK = GND
+ D1   = BLU YLW = Tx 
+ D2   = GRN WHT = Rx 
+ ---------------------------
+*/
+
+SoftwareSerial mySerial(D2, D1); // RX, TX
+
+unsigned char data[4] = {};
+
+void setup() {
+  Serial.begin(115200);
+  mySerial.begin(9600); 
+}
+
+float readDistance() {
+  do {
+    for (int i = 0; i < 4; i++) {
+      data[i] = mySerial.read();
+    }
+  } while (mySerial.read() == 0xff);
 
   mySerial.flush();
 
-  if(data[0]==0xff){
-      int sum;
-      sum=(data[0]+data[1]+data[2])&0x00FF;
-      if(sum==data[3])  {
-        distance=(data[1]<<8)+data[2];
-        if(distance>30)  {
-           distance= distance/10;
-          // Serial.print("distance="+ String(distance) + "cm");
-          } else  {
-            Serial.println("Below the lower limit");
-            distance = 0;
-          }
-      } else {
-      //  distance = -1;
-        Serial.println("Error!");
-      }
-     }
+  if (data[0] == 0xff) {
+    int sum = (data[0] + data[1] + data[2]) & 0x00FF;
+    if (sum == data[3]) {
+      float distance = (data[1] << 8) + data[2];
+      return distance / 10;
+    }
+  }
 
-  return  distance;
-}
-void setup()
-{
- Serial.begin(9600);
- mySerial.begin(9600); 
+  return -1;
 }
 
-void loop()
-{
-    Serial.println(getDistance());
-    delay(100);
+void loop() {
+  float distance = readDistance();
+  if (distance >=0) {
+    Serial.println(distance);
+  }  
+
+  delay(100);
 }
