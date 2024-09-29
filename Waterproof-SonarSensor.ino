660333dc76396683a1e906a808c328ceb7a33b00
Balan S.P <balansp@users.noreply.github.com>
2024-09-29 07:59:18 +0530

    Update Waterproof-SonarSensor.ino

--- Waterproof-SonarSensor.ino
+++ Waterproof-SonarSensor.ino
@@ -1,7 +1,3 @@
-#include <Arduino.h>
-#include <Wire.h>
-#include <SoftwareSerial.h>
-
 /*
 ---------------------------
 | ESP  |  WIRE  |  SONAR  |
@@ -12,43 +8,54 @@
 | D2   |  GRN   |  Rx     |
 ---------------------------
 */
+#include <Arduino.h>
+#include <Wire.h>
+#include <SoftwareSerial.h>
 
-SoftwareSerial mySerial(D2, D1); // RX, TX
+#define SONAR_RX D2
+#define SONAR_TX D1
+SoftwareSerial sonar(SONAR_RX, SONAR_TX);
 
 unsigned char data[4] = {};
 
-void setup() {
-  pinMode(D0, OUTPUT);
-  digitalWrite(D0, HIGH);
-  Serial.begin(115200);
-  mySerial.begin(9600);
+float readDistance() {
+    float distance = -1;
+    if (sonar.available() >= 4) {
+        for (int i = 0; i < 4; i++) {
+            data[i] = sonar.read();
+        }
+
+        sonar.flush();
+
+        if (data[0] == 0xff) {
+            int sum = (data[0] + data[1] + data[2]) & 0x00FF;
+            if (sum == data[3]) {
+                distance = (data[1] << 8) + data[2];
+                if (distance > 30) {
+                    return distance / 10;
+                }
+            }
+        }
+    }
+    return distance;
 }
 
-float readDistance() {
-  if (mySerial.available() >= 4) {
-    for (int i = 0; i < 4; i++) {
-      data[i] = mySerial.read();
-    }
-    if (data[0] == 0xff) {
-      int sum = (data[0] + data[1] + data[2]) & 0xFF;
-      if (sum == data[3]) {
-        int distance = (data[1] << 8) + data[2];
-        return distance / 10.0;
-      }
-    }
-  }
-
-  return -1;
+void setup() {
+    pinMode(D0, OUTPUT);
+    digitalWrite(D0, HIGH);
+    Serial.begin(115200);
+    sonar.begin(9600);
+    delay(1000);
 }
 
 void loop() {
-  float distanceInCm = readDistance();
-  
-  if (distanceInCm >= 0) {
-    Serial.println(distanceInCm);
-  } else {
-    Serial.println("Invalid data");
-  }
+    float distance = readDistance();
+    if (distance > 0) {
+        Serial.print("Distance: ");
+        Serial.print(distance);
+        Serial.println(" cm");
+    } 
 
-  delay(100);
+    delay(500);
 }
+
